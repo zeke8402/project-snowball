0a64328bcaf5bb76f72d59c4427235a1bf59959f
John Ezekiel <jkezekiel@gmail.com>
2016-07-26 06:00:09 +0000

    Removing node modules from tracking

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 var express = require('express');
+var Sequelize = require('sequelize');
 var app = express();
+var sequelize = new Sequelize('vagrant', 'vagrant', 'vagrant', {
+  host: 'localhost',
+  dialect: 'postgres',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+});
+
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
