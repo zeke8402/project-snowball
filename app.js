593ed08b8ce58ef7b30fd644aadbae2d82522f79
John Ezekiel <jkezekiel@gmail.com>
2016-10-23 23:32:16 +0000

    Adding API Module, thinking ahead. Also figured out how to pass local vars to pug

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 require('dotenv').config();
 
 var express = require('express'); // framework
-var session = require('express-session');
-var passport = require('passport'); // open id
-var util = require('util'); // idk
 var morgan = require('morgan'); // no idea
 var methodOverride = require('method-override'); // sure
+var passport = require('passport'); // open id
+var session = require('express-session');
+var util = require('util'); // idk
+
 var app = express();
 
+// Don't forget that this doesn't include itself in the path so /public/assets means /assets
 app.use(express.static('public'));
+app.set('view engine', 'pug');
 
 // Routing to node modules scripts
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
-app.use(morgan('dev')); // debugging im guessing
+app.use(morgan('dev'));
+
+// Modularizing the api
+var api = require('./lib/api');
+app.use('/api', api);
 
 var routes = require('./routes/index');
 app.use('/', routes);
---
+++ routes/api.js
@@ -0,0 +1,8 @@
+var app = require('../app');
+var express = require('express');
+var router = express.Router();
+
+// This will define routes for accessing the information we will collect
+router.get('/', function(req, res) {
+  console.log("Nothing here...");
+});
